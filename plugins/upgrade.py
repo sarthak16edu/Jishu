28b4dfb44ca2856bce9dc311d3be8b6b78934b0a
sarthak16edu <165332174+sarthak16edu@users.noreply.github.com>
2024-03-30 00:37:27 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -25,9 +25,9 @@ async def upgrade(bot,update):
 	After Payment Send Screenshots Of 
         Payment To Admin @calladminrobot"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/calladminrobot")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://telegra.ph/file/7f959437f9375b313ed1c.jpg"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://telegra.ph/file/30b3e45a02766803883cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("𝐀𝐃𝐌𝐈𝐍",url = "https://t.me/helpsarthak_bot")], 
+        			[InlineKeyboardButton("𝐏𝐇𝐎𝐍𝐄 𝐏𝐄",url = "https://telegra.ph/file/b6629cf65e9da7b2bcf80.jpg"),
+        			InlineKeyboardButton("𝐒𝐔𝐏𝐏𝐎𝐑𝐓",url = "https://t.me/sarthakkale16")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
